843c7566f4052559ec2a741b64c83473358fd289
Chris Page <chris_pagey100@hotmail.com>
2019-02-19 17:36:45 +0100

    Updated Version and Icon

--- SingleFieldCCSCalc/SingleFieldCCSCalc/My Project/AssemblyInfo.vb
+++ SingleFieldCCSCalc/SingleFieldCCSCalc/My Project/AssemblyInfo.vb
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.8.28868")>
+<Assembly: AssemblyVersion("0.0.10.29871")>
